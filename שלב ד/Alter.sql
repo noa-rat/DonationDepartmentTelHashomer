4d8a08ffe4334d89f185a483c92a5e082e87e14c
adinabc <adinabc24@gmail.com>
2025-06-19 22:02:16 +0300

    Update Alter.sql

--- שלב ד/Alter.sql
+++ שלב ד/Alter.sql
@@ -5,3 +5,90 @@ ADD COLUMN stock INT DEFAULT 100;
 
 ALTER TABLE fundraisingevent
 ADD COLUMN e_fundraising_goal INT DEFAULT 20000;
+
+
+CREATE TABLE maximum_amount_of_patients
+(
+	type_of_doctor VARCHAR NOT NULL,
+	max_patients INT NOT NULL,
+	PRIMARY KEY (type_of_doctor)
+)
+INSERT INTO maximum_amount_of_patients (type_of_doctor, max_patients)
+VALUES
+    ('doctor', 20),
+    ('surgeon', 8),
+    ('header doctor', 0),
+	('attending', 20),
+	('resident', 10),
+	('trainee', 5),
+	('on-call', 0);
+
+CREATE SEQUENCE IF NOT EXISTS public.donation_donation_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+SELECT MAX(donation_id) FROM donation;
+SELECT setval('donation_donation_id_seq', 10000, true);
+
+ALTER TABLE Donation
+ALTER COLUMN donation_id SET DEFAULT nextval('donation_donation_id_seq');
+
+CREATE SEQUENCE IF NOT EXISTS public.donor_donor_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+SELECT MAX(donor_id) FROM donor;
+SELECT setval('donor_donor_id_seq', 10000, true);
+
+ALTER TABLE Donor
+ALTER COLUMN donor_id SET DEFAULT nextval('donor_donor_id_seq');
+
+CREATE SEQUENCE IF NOT EXISTS public.fundraisingevent_e_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+SELECT MAX(e_id) FROM fundraisingevent;
+SELECT setval('fundraisingevent_e_id_seq', 10000, true);
+
+ALTER TABLE fundraisingevent
+ALTER COLUMN e_id SET DEFAULT nextval('fundraisingevent_e_id_seq');
+
+CREATE SEQUENCE IF NOT EXISTS public.patient_patient_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
+
+SELECT MAX(patient_id) FROM patient;
+SELECT setval('patient_patient_id_seq', 10000, true);
+
+ALTER TABLE patient
+ALTER COLUMN patient_id SET DEFAULT nextval('patient_patient_id_seq');
+
+CREATE SEQUENCE IF NOT EXISTS public.project_p_id_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+SELECT MAX(p_id) FROM project;
+SELECT setval('project_p_id_seq', 10000, true);
+
+ALTER TABLE project
+ALTER COLUMN p_id SET DEFAULT nextval('project_p_id_seq');
+
+
+
+
+
